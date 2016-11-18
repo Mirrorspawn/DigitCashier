c87b31c8cb8e1ed8add4af7dbd6c61dc1e6760ff
Johannes Svensson <mirrorspawn@gmail.com>
2016-11-18 12:26:13 +0100

    Added the class Item with a Constructor method and methods to get the values of the attributes.

--- src/se/humanus/DigitCashier/Item.java
+++ src/se/humanus/DigitCashier/Item.java
@@ -1,5 +1,39 @@
 package se.humanus.DigitCashier;
 
 public class Item {
+	//Class to handle items for purchase. /JS
+	private int idNr; // unique identifying two-digit number
+	private String name; // the name of the item
+	private double price; // the price of the iteem
+	private double discountModifier=1.0; //The number you have to multiply the price by to get the discounted price
+	
+	public double getPrice () {
+		return price;
+		}
+	
+	public int getId () {
+		return idNr;
+		}
+	
+	public String getName () {
+		return name;
+		}
+	
+	public double getDiscount () {
+		return discountModifier;
+		}
+	
+	@Override
+	public String toString() {
+		//returns information about the item in String format
+		return idNr + " " + name + " " + price;
+		}
+	
+	public Item (int idNr, String name, double price) {
+		//Standard constructor method, does not set discount modifier
+		this.idNr = idNr;
+		this.name = name;
+		this.price = price;
+		}
 
 }
