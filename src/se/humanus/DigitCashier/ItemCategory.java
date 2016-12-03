64f0660984b03a4f2734c132a23fedc917dec96e
awe.fendi@gmail.com <awe.fendi@gmail.com>
2016-12-03 23:51:35 +0100

    Måste ta Commit/ Push på dessa filer (CashRegisterWindow, ItemCategory och Login) för att få GIT att fungera

---
+++ src/se/humanus/DigitCashier/ItemCategory.java
@@ -0,0 +1,50 @@
+package se.humanus.DigitCashier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemCategory {
+	String categoryName;
+	Float salesTaxMod=1.12f;
+	Boolean measuredInWeight;
+	private List<Item> itemsInCategory = new ArrayList<>(); 
+	
+	public String getCategoryName() {
+		return categoryName;
+	}
+	
+	public float getSalesTax() {
+		return salesTaxMod;
+	}
+	
+	public Boolean getMeasuredInWeight() {
+		return measuredInWeight;
+	}
+	
+	public void addItemToCategory(Item item) {
+		itemsInCategory.add(item);		
+	}
+	
+	public void removeItemFromCategory(Item item) { //Not tested. Removes an item from the category's list of items.
+		if(itemsInCategory.contains(item)) {
+			itemsInCategory.remove(itemsInCategory.indexOf(item));
+		}
+	}
+	
+	public boolean isItemInCategory(Item item) {
+		boolean itemPresent=false;
+		for (Item i:itemsInCategory){
+			if (i==item){
+				itemPresent=true;
+			}
+		}
+		return itemPresent;
+	}
+	
+	public ItemCategory(String name, float taxMod, Boolean weight) {
+		this.categoryName=name;
+		this.salesTaxMod=taxMod;
+		this.measuredInWeight=weight;
+	}
+
+}
