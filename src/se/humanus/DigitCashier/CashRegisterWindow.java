7db1c7880438155eb88e7769c5bb3d4733c42aad
awe.fendi@gmail.com <awe.fendi@gmail.com>
2016-12-04 21:20:47 +0100

    Code for #2# function with comments. AF

--- src/se/humanus/DigitCashier/CashRegisterWindow.java
+++ src/se/humanus/DigitCashier/CashRegisterWindow.java
@@ -125,22 +125,17 @@ public class CashRegisterWindow {
 				String antalMatch = "[0-9]{1,3}"; //Sets the regular expression for controlling what you can put into antal.
 				String inputString = varuNrText.getText(); //gets the content of the inputwindow for varuNr
 				
-			
-//				public static float showFinalSum {
-//					component.getInputMap().put(KeyStroke.getKeyStroke("#2#"),
-//			                "amountOfItemList");
-//			component.getActionMap().put("amountOfItemList",
-//			                 anAction);
-//
-//					}
+		
 				
-				if(inputString.equals("#2#")) {
+				if(inputString.equals("#2#")) { // Code for the #2# function. AF
 					System.out.println("show total items here..");
 					double sum = CashRegister.calculateSum();
-					int a = (int) (sum * 100);
+					int a = (int) (sum * 100); // Code to limit the decimal numbers to 2. Code by my brother
 					sum = a / 100.0;
 					lblDisplay.setText("total sum = " + sum);
 					return;
+				
+				
 				}
 				if (!antalString.matches(antalMatch)) {
 					lblDisplay.setText("Ogiltig mängd/vikt");
