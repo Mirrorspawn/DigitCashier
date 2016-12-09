046902150f506f51b2cf9402983c4c6dd6d6c5e9
Patrik Gullberg <Patrikgul@hotmail.com>
2016-12-09 13:34:16 +0100

    adds login window and connects it to Cashregister

--- src/se/humanus/DigitCashier/Login.java
+++ src/se/humanus/DigitCashier/Login.java
@@ -5,83 +5,14 @@ import java.util.Scanner;
 public class Login {
 
 	public static void main(String[] args) {
-		
-		/*Scanner input = new Scanner(System.in);
-		
-		String user="", pass="", EmployeeID="";
-		
-		
-		
-      
-		boolean validName=false;
-		while(!validName){
-		System.out.print("Input username: ");
-		user = input.nextLine();
-		
-		if (user.equals("Kassör") || (user.equals("Admin") || (user.equals("Chef"))))
-			validName=true;
-		
-		else {
-			
-			System.out.println("Invalid user");
-			
-		}
-		}
-		
-		boolean ValidEmployeeID=false;
-		
-		while(!ValidEmployeeID){
-		System.out.print("Input Employee ID: ");
-		EmployeeID = input.nextLine();
-		
-		if (EmployeeID.equals("01") && (user.equals("Kassör"))){ //(EmployeeID.equals("02") || (EmployeeID.equals("03"))))
-			ValidEmployeeID=true;
-		}
-		
-		else if (EmployeeID.equals("02") && (user.equals("Admin"))){
-			ValidEmployeeID=true;
-		}
-		
-		else if (EmployeeID.equals("03") && (user.equals("Chef"))){
-			ValidEmployeeID=true;
-		}
-		
-			
-			else 
-			{
-			
-			System.out.println("Invalid Employee ID");
-		}
-		}
-		
-		boolean ValidPassword=false;
-		while(!ValidPassword){
-		System.out.print("Input Password: ");
-		pass = input.nextLine();
-		
-		if (pass.equals("201") && (EmployeeID.equals("01"))) {
-			ValidPassword=true;
-		}
-		else if (pass.equals("302") && (EmployeeID.equals("02"))) {
-			ValidPassword=true;
-		}
-		
-		else if (pass.equals("303") && (EmployeeID.equals("03"))) {
-			ValidPassword=true;
-		}
-		else {
-				
-				System.out.println("Invalid Password");
-			}
-		System.out.println("Du har blivit inloggad.");
-			
-		}
-	input.close();
+		LoginGUI.activateLoginWindow();
+	}
 		
 		/*Method-calls below initialize and starts things to do with CashRegister. 
 		 * Any login code should probably be before that call. Any call to a method that needs to use the 
 		 *the hardcode-created Items should be put after it /JS 		
 		 */
+	public static void startApplication(){ 	
 		CashRegister.initializeCategories(); // This is a call to a method in Cashregister that creates some objects of the class ItemCategory./JS
 		CashRegister.initializeItems(); //This is a call to a method in CashRegister that creates twenty objects of the class Item./JS
 		CashRegisterWindow.activateCashRegister(); //Starts up the CashRegister interface.
---
+++ src/se/humanus/DigitCashier/LoginGUI.java
@@ -0,0 +1,124 @@
+package se.humanus.DigitCashier;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.widgets.Label;
+
+public class LoginGUI {
+
+	protected Shell LoginWindow;
+	private Text usernameField;
+	private Text passwordField;
+	
+	CashRegisterWindow cashRegisterGUI = new CashRegisterWindow();
+	
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 * @wbp.parser.entryPoint
+	 */
+	//public static void main(String[] args) {
+	public static void activateLoginWindow(){
+		try {
+			LoginGUI window = new LoginGUI();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		LoginWindow.open();
+		LoginWindow.layout();
+		while (!LoginWindow.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		LoginWindow = new Shell();
+		LoginWindow.setSize(450, 300);
+		LoginWindow.setText("SWT Application");
+		
+		Label InvalidUsername = new Label(LoginWindow, SWT.NONE);
+		InvalidUsername.setBounds(201, 53, 97, 15);
+		InvalidUsername.setText("Username");
+		
+		Label invalidPassword = new Label(LoginWindow, SWT.NONE);
+		invalidPassword.setBounds(201, 105, 97, 15);
+		invalidPassword.setText("Password");
+		
+		usernameField = new Text(LoginWindow, SWT.BORDER);
+		usernameField.setBounds(54, 43, 141, 32);
+		
+		passwordField = new Text(LoginWindow, SWT.BORDER);
+		passwordField.setBounds(54, 95, 141, 32);
+		
+		Button btnLogin = new Button(LoginWindow, SWT.NONE);
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				String username = usernameField.getText();
+				String password = passwordField.getText();
+				if (username.equals("Kassör")){
+					if (password.equals("201")){
+						
+						Login.startApplication();
+						
+					}
+					else{
+						invalidPassword.setText("Invalid Password");	
+					}
+				}
+				else if (username.equals("Admin")){
+					if (password.equals("302")){
+						
+						Login.startApplication();
+						
+					}
+					else{
+						invalidPassword.setText("Invalid login-code");
+					}
+				
+				}
+				else if (username.equals("Chef")){
+					if (password.equals("503")){
+						
+						Login.startApplication();
+					}	
+					else{
+						invalidPassword.setText("Invalid login-code");
+					}
+					
+				}
+				else{
+					InvalidUsername.setText("Invalid Username");
+					
+				}
+			}
+		});
+		btnLogin.setBounds(75, 156, 75, 25);
+		btnLogin.setText("Login");
+
+	}
+}
